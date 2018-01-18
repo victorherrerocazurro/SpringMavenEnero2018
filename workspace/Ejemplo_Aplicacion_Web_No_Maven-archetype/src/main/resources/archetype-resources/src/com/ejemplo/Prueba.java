4052f9de288d2340e6e1f8a604bbe30fb23f5b36
Westcon-Comstor Training <Training@71W71135.eu.westcongrp.com>
2018-01-18 08:37:34 +0100

    Dia 1 - Maven: Arquetipos, Ciclo de Vida, Fases, Goals, Herencia

---
+++ workspace/Ejemplo_Aplicacion_Web_No_Maven-archetype/src/main/resources/archetype-resources/src/com/ejemplo/Prueba.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+class Prueba {
+
+	@Test
+	void test() {
+		Assert.fail("Not yet implemented");
+	}
+
+}
---
+++ workspace/Replica_Ejemplo_No_Web/src/com/ejemplo/Prueba.java
@@ -0,0 +1,15 @@
+package com.ejemplo;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+class Prueba {
+
+	@Test
+	void test() {
+		Assert.fail("Not yet implemented");
+	}
+
+}
